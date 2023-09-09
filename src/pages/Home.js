a2225165402c817cfa922353f8c3ab67fa339ccc
mahimagupta24 <mahimagupta2401@gmail.com>
2023-09-09 18:43:28 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return <div className="home-main">
+        <div className="home-container">
+    <h1>Your Unique Component Library</h1>
+    <p className="home-para">A collection of innovative components for modern web develpoment</p>
+    </div>
+    </div>
+}
