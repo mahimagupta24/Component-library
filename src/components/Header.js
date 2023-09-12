93b2c1382c3ce194d3b6c02b090847507bc26035
mahimagupta24 <mahimagupta2401@gmail.com>
2023-09-12 10:32:01 +0530

    add search container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate =useNavigate()
+  const [text, setText] = useState("");
+
+ const componentSearchHandler=()=>{
+  const searchedComponent=text.toLowerCase()
+  navigate(`/${searchedComponent}`)
+ }
   return (
     <div>
       <div className="nav-heading">
-        <h1>NEOG'S COMPONENT LIBRARY</h1>
+        <h1>Neog's Component Library</h1>
+        <div className="search-container">
+          <input value={text} onChange={(e) => setText(e.target.value)} placeholder="search your component here" />
+         <button className="search-btn"onClick={componentSearchHandler}>Search</button>
+        </div>
         <div className="nav-container">
           <Link className="link" to="/">
             Home
